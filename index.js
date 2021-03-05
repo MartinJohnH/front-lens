ce8a5334cbdfea459937b648c164dcbf387fa144
mjhearty <martinjohn.hearty@gmail.com>
2021-03-04 20:31:18 -0500

    format code

--- index.js
+++ index.js
@@ -6,70 +6,56 @@ export function useFrontLens(projectDir, webstormLocalhost) {
     const [isShiftPressed, setIsShiftPressed] = useState(false);
     const [hoveredElement, setHoveredElement] = useState(null);
 
-    function handleOnClickTest(e) {
-        let objectDebugInstance = Object.values(e.target)[0];
-        if ((e.metaKey || e.ctrlKey) && Object.values(objectDebugInstance)[29]) {
+    function getWebstormURL(objectDebugInstance) {
+        if (Object.values(objectDebugInstance)[29]) {
             let elementEditorInfo = Object.values(objectDebugInstance)[29];
-            if (elementEditorInfo) {
-                //console.log(Object.values(objectDebugInstance));
-                let elementURL = elementEditorInfo["fileName"].split(projectDir)[1];
-                let myWindow = window.open(webstormLocalhost + "/api/file/" + elementURL + ":" + elementEditorInfo.lineNumber + ":" + elementEditorInfo.columnNumber);
-                let timer  = setTimeout(function(){
-                    myWindow.close();
-                }, 300);
+            let codeFileURL = elementEditorInfo["fileName"].split(projectDir)[1];
+            let codeFileLineNum = elementEditorInfo.lineNumber;
+            let codeFileColNum = elementEditorInfo.columnNumber;
+            return (webstormLocalhost + "/api/file/" + codeFileURL + ":" + codeFileLineNum + ":" + codeFileColNum);
+        }
+        return null;
+    }
 
-                return () => {
-                    clearTimeout(timer);
-                }
+    function handleOnClickTest(e) {
+        let webstormURL = getWebstormURL(Object.values(e.target)[0]);
+
+        if (webstormURL && (e.metaKey || e.ctrlKey)) {
+            let myWindow = window.open(webstormURL);
+            let timer  = setTimeout(function() {
+                myWindow.close();
+            }, 300);
+            return () => {
+                clearTimeout(timer);
             }
-        }
-    }
-
-    function handleOnCMDDown(e) {
-        if (e.metaKey || e.ctrlKey) {
-            setIsCMDPressed(true);
-        }
-        if (e.shiftKey) {
-            setIsShiftPressed(true);
-        }
-    }
-
-    function handleOnCMDUp(e) {
-        //e.keyCode === 224 -> command key
-        if (e.isComposing ||e.keyCode === 224) {
-            setIsCMDPressed(false);
-        }
-        //e.keyCode === 16 -> shift key
-        if (e.isComposing ||e.keyCode === 16) {
-            setIsShiftPressed(false);
         }
     }
 
     function handleMouseMove(e) {
         let hoverElement = document.elementFromPoint(e.clientX, e.clientY);
         setHoveredElement(hoverElement);
-        if(e.metaKey || e.ctrlKey) {
+        //if cmd or ctrl key is pressed
+        if (e.metaKey || e.ctrlKey) {
             hoverElement.setAttribute('data-element-name', hoverElement.tagName);
         } else {
             hoverElement.removeAttribute('data-element-name');
         }
     }
 
-    useEffect(() => {
-        if(isShiftPressed && hoveredElement && hoveredElement.getAttribute('class')){
-            hoveredElement.setAttribute(
-                'data-class-name',
-                "." + hoveredElement.getAttribute('class')
-            );
-        } else {
-            if(hoveredElement){
-                hoveredElement.removeAttribute('data-class-name');
-            }
-        }
-    },[isShiftPressed]);
+    function handleOnCMDDown(e) {
+        if (e.metaKey || e.ctrlKey) setIsCMDPressed(true);
+        if (e.shiftKey) setIsShiftPressed(true);
+    }
+
+    function handleOnCMDUp(e) {
+        //e.keyCode === 224 -> command key
+        if (e.isComposing ||e.keyCode === 224) setIsCMDPressed(false);
+        //e.keyCode === 16 -> shift key
+        if (e.isComposing ||e.keyCode === 16) setIsShiftPressed(false);
+    }
 
     useEffect(() => {
-        if(isCMDPressed){
+        if (isCMDPressed) {
             document.body.classList.add("cmd-select-style");
         } else {
             document.body.classList.remove("cmd-select-style");
@@ -77,15 +63,28 @@ export function useFrontLens(projectDir, webstormLocalhost) {
     },[isCMDPressed]);
 
     useEffect(() => {
+        if (isShiftPressed && hoveredElement) {
+            if (hoveredElement.getAttribute('class')) {
+                hoveredElement.setAttribute(
+                    'data-class-name',
+                    "." + hoveredElement.getAttribute('class')
+                );
+            }
+        } else {
+            if (hoveredElement) hoveredElement.removeAttribute('data-class-name');
+        }
+    },[isShiftPressed]);
+
+    useEffect(() => {
         document.addEventListener("click", handleOnClickTest);
+        document.addEventListener("mousemove",handleMouseMove);
         document.addEventListener("keydown",handleOnCMDDown);
         document.addEventListener("keyup",handleOnCMDUp);
-        document.addEventListener("mousemove",handleMouseMove);
         return () => {
             document.removeEventListener("click", handleOnClickTest);
+            document.removeEventListener("mousemove",handleMouseMove);
             document.removeEventListener("keydown",handleOnCMDDown);
             document.removeEventListener("keyup",handleOnCMDUp);
-            document.removeEventListener("mousemove",handleMouseMove);
         };
     },[]);
 }
